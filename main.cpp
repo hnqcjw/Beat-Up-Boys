7696d82bddd994add082917a7130468a18eec587
si <hnqcjw.xvc@gmail.com>
2025-09-16 16:08:31 -0700

    First Commit

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+using namespace sf;
+
+int main() {
+    RenderWindow window(VideoMode(800, 600), "Super Smash Bros, Recreated");
+
+    enum state { title, charSelect, stageSelect, fight };
+
+    Font font;
+    if (!font.loadFromFile("./assets/tuffy.ttf")) {
+        std::cerr << "No Font";
+        return EXIT_FAILURE;
+    }
+
+    Color currentBgColor = Color::Blue;
+
+    state currentState = title;
+
+    // Set up title text!
+
+    Text titleScreen("\t\t\t\tSuper Smash Bros\n\n\n\n\n\n\n\n\n\n\t\t\t\tPress A to play", font, 30);
+    titleScreen.setStyle(Text::Bold);
+    titleScreen.setFillColor(Color::White);
+
+    Text demoText("Fighting: Coming Soon!", font, 30);
+    demoText.setFillColor(Color::Red);
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+
+        // Handle real-time input
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+
+        window.clear(currentBgColor);
+        if (currentState == title && Keyboard::isKeyPressed(Keyboard::A)) {
+            currentState = charSelect;
+        }
+
+        if (currentState == title)  {
+            window.draw(titleScreen);
+            currentBgColor = Color::Blue;
+        }
+        else if (currentState == charSelect) {
+            window.draw(demoText);
+            currentBgColor = Color::Black;
+        }
+        window.display();
+    }
+
+    return 0;
+}
+
